3d4341e2a1f86f1f2c7d256f64d55a72634209f9
Eugene Yokota <eed3si9n@gmail.com>
2026-08-20 18:28:24 -0400

    [1.x] Bump IO to 1.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     sys.env.get("BUILD_VERSION") orElse sys.props.get("sbt.build.version")
 
   // sbt modules
-  private val ioVersion = nightlyVersion.getOrElse("1.12.2")
+  private val ioVersion = nightlyVersion.getOrElse("1.13.0")
   private val lmVersion =
     sys.props.get("sbt.build.lm.version").orElse(nightlyVersion).getOrElse("1.12.3")
   val zincVersion = nightlyVersion.getOrElse("1.12.1")
